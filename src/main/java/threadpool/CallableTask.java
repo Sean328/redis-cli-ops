902dc7b023a6997afe37792b237b5f28d3b5f245
sean <seanlee2333@gmail.com>
2018-12-25 21:56:26 +0800

    新增线程池工具类

---
+++ src/main/java/threadpool/CallableTask.java
@@ -0,0 +1,53 @@
+package threadpool;
+
+import common.ReflectionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.util.Map;
+import java.util.concurrent.Callable;
+
+/**
+ * 重写Callable的实现类
+ * @author lixin
+ * @date 2018-12-25 10:16
+ **/
+public class CallableTask<T> implements Callable {
+    private static final Logger logger = LoggerFactory.getLogger(CallableTask.class);
+
+    private Object clazz;
+    private String methodName;
+    private Object[] fields;
+    private Map<String,Object> taskContext;
+
+    public CallableTask(){
+      this.clazz = null;
+      this.methodName = "";
+      logger.debug("");
+    }
+
+    public CallableTask(Object clazz,String methodName){
+        this.clazz = clazz;
+        this.methodName = methodName;
+        logger.debug("");
+    }
+
+    public CallableTask(Object clazz,String methodName,Object ...fields){
+        this.clazz = clazz;
+        this.methodName = methodName;
+        this.fields = fields;
+        logger.debug("");
+    }
+
+    @Override
+    public T call() throws Exception {
+        try {
+            logger.debug("开始执行callable任务：[class:{},methodName:{},fields:{}]",this.clazz,this.methodName,this.fields);
+            T result = ReflectionUtils.invokMethod(this.clazz,this.methodName,this.fields);
+            logger.debug("执行任务结束:[{}#{}]",clazz,methodName);
+            return result;
+        }catch (Exception e){
+            logger.error("执行任务失败，当前任务：[{}#{}]",clazz,methodName,e);
+            throw e;
+        }
+    }
+}
---
+++ src/main/java/threadpool/ThreadPoolService.java
@@ -0,0 +1,68 @@
+package threadpool;
+
+import java.util.concurrent.*;
+
+/**
+ * @author lixin
+ * @date 2018-12-25 15:46
+ **/
+public class ThreadPoolService {
+
+    private ThreadPoolService threadPoolService;
+
+    //阻塞队列
+    private BlockingQueue<Runnable> workQueue = new ArrayBlockingQueue<>(1000);
+    //核心线程size
+    private Integer corePoolSize = 10;
+    //最大可执行线程size
+    private Integer maximumPoolSize = 30;
+    //闲置线程存活时间
+    private Long keepAliveTime = 60L;
+    //拒绝策略
+    private RejectedExecutionHandler rejectHandler = new ThreadPoolExecutor.CallerRunsPolicy();
+    //默认线程池工厂
+    private ThreadFactory threadFactory = new ThreadFactory() {
+        @Override
+        public Thread newThread(Runnable r) {
+            return null;
+        }
+    };
+
+    private ThreadPoolExecutor defaultThreadPool = new ThreadPoolExecutor(corePoolSize, maximumPoolSize, keepAliveTime, TimeUnit.SECONDS,
+            workQueue,new DefaultThreadFactory(),rejectHandler);
+
+    private ThreadPoolService() {
+    }
+
+    public static ThreadPoolService getInstance() {
+        return InstanceHolder.threadPoolService;
+    }
+
+    /**
+     * 静态内部类初始化对象
+     */
+    private static class InstanceHolder {
+        public static ThreadPoolService threadPoolService = new ThreadPoolService();
+    }
+
+
+
+
+    public void submit(RunnableTask runnableTask) {
+        if (null == runnableTask) {
+            throw new IllegalArgumentException("任务不能为空");
+        }
+
+        defaultThreadPool.submit(runnableTask);
+    }
+
+
+    public Future submitWithReturn(CallableTask callableTask) {
+        if (null == callableTask) {
+            throw new IllegalArgumentException("任务不能为空");
+        }
+
+        return defaultThreadPool.submit(callableTask);
+    }
+
+}
